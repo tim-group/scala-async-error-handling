f63285ddc9573fea92db98c5eb61ab61c0181dd5
Marc Siegel <marc.siegel@timgroup.com>
2014-03-11 11:53:33 -0400

    Refine/clarify existing specs of Either-to-Future conversions
      *  Factor out and rename common vals
      *  Remove unnecessary "should"s from spec titles

--- src/test/scala/com/timgroup/asyncerrorhandling/EitherConvertersSpec.scala
+++ src/test/scala/com/timgroup/asyncerrorhandling/EitherConvertersSpec.scala
@@ -4,32 +4,27 @@ import org.scalatest.FunSpec
 import org.scalatest.Matchers
 import org.scalatest.concurrent.ScalaFutures
 
-import org.scalautils.Bad
-import org.scalautils.Good
-import org.scalautils.Or
-
 class EitherConvertersSpec extends FunSpec with Matchers with ScalaFutures {
-  describe("EitherFuture converter") {
-    it("should convert a Left into a Future with a SemanticError") {
-      val bad: Either[String, Int] = Left("error")
+  type ErrorMessage = String
+  val right: Either[ErrorMessage, Int] = Right(42)
+  val left:  Either[ErrorMessage, Int] = Left("error")
 
-      val f = EitherConverters.toFuture(bad)
+  describe("Either to Future conversions") {
+    it("converts a Left to a Future which is failed with a SemanticError") {
+      val f = EitherConverters.toFuture(left)
       f.failed.futureValue should be (SemanticError("error"))
     }
 
-    it("should convert Right into a Future with the value") {
-      val good: Either[String, Int] = Right(42)
-
-      val f = EitherConverters.toFuture(good)
+    it("converts a Right to a Future which is successful with the value") {
+      val f = EitherConverters.toFuture(right)
       f.futureValue should be (42)
     }
 
-    it("should be able to use the implicits") {
-      val good: Either[String, Int] = Right(42)
-
+    it("provides an implicit to enrich an Either with a conversion method to Future") {
       import EitherConverters.EitherFutureConverter
-      val f = good.toSemanticErrorFuture
+      val f = right.toSemanticErrorFuture
       f.futureValue should be (42)
     }
   }
+
 }
