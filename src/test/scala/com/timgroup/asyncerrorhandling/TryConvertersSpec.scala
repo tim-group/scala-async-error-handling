47abbfcc1daf9c021fb8683d3dcf444b077954d0
Marc Siegel <marc.siegel@timgroup.com>
2014-03-11 12:00:15 -0400

    Refine/clarify existing specs of Try-to-Future conversions
      *  Factor out and rename common vals
      *  Remove unnecessary "should"s from spec titles

--- src/test/scala/com/timgroup/asyncerrorhandling/TryConvertersSpec.scala
+++ src/test/scala/com/timgroup/asyncerrorhandling/TryConvertersSpec.scala
@@ -1,39 +1,34 @@
 package com.timgroup.asyncerrorhandling
 
-
-import org.scalatest.FunSpec
-import org.scalatest.Matchers
-import org.scalatest.concurrent.ScalaFutures
-import org.scalautils.Bad
-import org.scalautils.Good
-import org.scalautils.Or
 import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
 
-class TryConvertersSpec extends FunSpec with Matchers with ScalaFutures {
-  describe("TryFuture converter") {
-    it("should convert a Failure into a Future with exception") {
-      val e = new RuntimeException("error")
-      val bad: Try[Int] = Failure(e)
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+import org.scalatest.concurrent.ScalaFutures
 
-      val f = TryConverters.toFuture(bad)
-      f.failed.futureValue should be (e)
+class TryConvertersSpec extends FunSpec with Matchers with ScalaFutures {
+  val ex = new RuntimeException("error")
+  val failure: Try[Int] = Failure(ex)
+  val success: Try[Int] = Success(42)
+
+  describe("Try to Future conversions") {
+    it("converts a Failure with an exception to a Future which is failed with the exception") {
+      val f = TryConverters.toFuture(failure)
+      f.failed.futureValue should be (ex)
     }
 
-    it("should convert Succuess into a Future with the value") {
-      val good: Try[Int] = Success(42)
-
-      val f = TryConverters.toFuture(good)
+    it("converts a Succuess to a Future which is successful with the value") {
+      val f = TryConverters.toFuture(success)
       f.futureValue should be (42)
     }
 
-    it("should be able to use the implicits") {
-      val good: Try[Int] = Success(42)
-
+    it("provides an implicit to enrich a Try with a conversion method to Future") {
       import TryConverters.TryFutureConverter
-      val f = good.toFuture
+      val f = success.toFuture
       f.futureValue should be (42)
     }
   }
+
 }
